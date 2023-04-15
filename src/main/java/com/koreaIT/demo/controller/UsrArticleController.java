d1bdd6008cc90117ff689cc266e8d97edcfcc56d
dohoboo20 <dohoboo20@gmail.com>
2023-04-15 18:24:25 +0900

    테스트데이터 생성

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -19,6 +19,20 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
+		
+	}
+
+	private void makeTestData() {
+		for(int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			Article article = new Article(i, title, body);
+			articles.add(article);
+		}
+		
 	}
 
 	@RequestMapping("/usr/article/doAdd")
