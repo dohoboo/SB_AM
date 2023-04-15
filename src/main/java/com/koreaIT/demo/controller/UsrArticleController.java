f9033c7b7c912699da3457f9ea0137f9863e66a7
dohoboo20 <dohoboo20@gmail.com>
2023-04-15 22:00:13 +0900

    게시글 검색 메서드 분리

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -19,71 +19,86 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
-		
+
 		makeTestData();
-		
 	}
 
 	private void makeTestData() {
-		
-		for(int i = 1; i <= 10; i++) {
+
+		for (int i = 1; i <= 10; i++) {
 			String title = "제목" + i;
 			String body = "내용" + i;
-			
+
 			wirteAritcle(title, body);
 		}
-		
 	}
-	
+
 	private Article wirteAritcle(String title, String body) {
-		
+
 		int id = lastArticleId + 1;
 		lastArticleId = id;
 
 		Article article = new Article(id, title, body);
 
 		articles.add(article);
-		
+
 		return article;
+	}
+
+	private Article getArticleById(int id) {
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
 	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		
+
 		Article article = wirteAritcle(title, body);
-		
+
 		return article;
 	}
-	
+
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
-		
+
+		Article article = getArticleById(id);
+
+		if (article == null) {
+			return id + "번 게시글은 존재하지 않습니다.";
+		}
+		articles.remove(article);
+		return id + "번 게시글이 삭제되었습니다.";
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+
 		Article foundArticle = null;
-		
-		for(Article article : articles) {
-			if(article.getId() == id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
 				foundArticle = article;
 				break;
 			}
-			
-			if(foundArticle == null) {
+			if (foundArticle == null) {
 				return id + "번 게시글은 존재하지 않습니다.";
 			}
-			
-			articles.remove(foundArticle);
+			foundArticle = wirteAritcle(title, body);
 		}
-		
-		return id + "번 게시글이 삭제되었습니다.";
+		return id + "번 게시글이 수정되었습니다.";
 	}
-	
-	
 
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		
+
 		return articles;
 	}
 }
